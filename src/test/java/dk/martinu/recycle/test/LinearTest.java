4ca987b1b983f3acc441480531fab50ce0ca208e
Adam Martinu <1337programmering.c@gmail.com>
2022-12-06 13:44:08 +0100

    Added license header

--- src/test/java/dk/martinu/recycle/test/LinearTest.java
+++ src/test/java/dk/martinu/recycle/test/LinearTest.java
@@ -1,12 +1,26 @@
-package dk.martinu.recycle.test;
+/*
+ * Copyright (c) 2021, Adam Martinu. All rights reserved. Altering or
+ * removing copyright notices or this file header is not allowed.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");  you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-import org.junit.jupiter.api.Test;
+package dk.martinu.recycle.test;
 
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import dk.martinu.recycle.*;
+import dk.martinu.recycle.Recyclers;
+import dk.martinu.recycle.RetentionPolicy;
 
 /**
  * Test class for linear recyclers.
