1164d15d118c94879d2ec12d0c86e989dd223bcc
Adam Martinu <1337programmering.c@gmail.com>
2022-12-15 19:25:55 +0100

    Added tests

--- src/test/java/dk/martinu/recycle/test/RecyclerStackTest.java
+++ src/test/java/dk/martinu/recycle/test/RecyclerStackTest.java
@@ -17,82 +17,259 @@
 
 package dk.martinu.recycle.test;
 
-import org.junit.jupiter.api.Test;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 import dk.martinu.recycle.PoolAny;
 import dk.martinu.recycle.RecyclerStack;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Test class for recycler stacks. Due to the close relation between
- * {@code Recycler} objects and {@code RecyclerStack} objects, most tests
- * related to stacks are done in {@link RecyclerTest}.
+ * Test class for recycler stacks.
  */
+@DisplayName("RecyclerStack")
 public class RecyclerStackTest {
 
-    // asserts remove() works as intended
-    @Test
-    public void remove() {
-        final RecyclerStack<Object> stack = new RecyclerStack<>(() -> new Object[128], PoolAny.get());
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("is empty after clear")
+    void emptyAfterClear(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+        assertNotEquals(0, stack.size());
 
-        // assert method can be called on empty stack
-        assertDoesNotThrow(() -> {
-            stack.remove(10);
-        });
-
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        assertEquals(3, stack.size());
-
-        // assert method can be called on non-empty stack
-        assertDoesNotThrow(() -> stack.remove(10));
+        stack.clear();
+        assertTrue(stack.isEmpty());
         assertEquals(0, stack.size());
+    }
 
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        assertEquals(4, stack.size());
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("is empty after clear (limited bucket size)")
+    void emptyAfterClear_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+        assertNotEquals(0, stack.size());
 
-        // assert method removes correct number of elements
-        assertDoesNotThrow(() -> stack.remove(1));
+        stack.clear();
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("is empty when new")
+    void isEmpty(@NotNull final RecyclerStack<Integer> stack) {
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("is not empty after pushing")
+    void isNotEmpty(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        assertEquals(1, stack.size());
+
+        stack.push(0);
+        assertEquals(2, stack.size());
+
+        stack.push(0);
         assertEquals(3, stack.size());
-        assertDoesNotThrow(() -> stack.remove(2));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("is not empty after pushing (limited bucket size)")
+    void isNotEmpty_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        assertEquals(1, stack.size());
+
+        stack.push(0);
+        assertEquals(2, stack.size());
+
+        stack.push(0);
+        assertEquals(3, stack.size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can pop")
+    void pop(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        assertFalse(stack.isEmpty());
+
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+        assertEquals(0, stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("pop() throws NoSuchElementException when empty")
+    void popThrowsWhenEmpty(@NotNull final RecyclerStack<Integer> stack) {
+        assertThrows(NoSuchElementException.class, stack::pop);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can pop (limited bucket size)")
+    void pop_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        assertFalse(stack.isEmpty());
+
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+        assertEquals(0, stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can push")
+    void push(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can push (limited bucket size)")
+    void push_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove when empty")
+    void removeEmpty(@NotNull final RecyclerStack<Integer> stack) {
+        stack.remove(10);
+        assertTrue(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument equal to size")
+    void removeExact(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+
+        stack.remove(3);
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument equal to size (limited bucket size)")
+    void removeExact_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+
+        stack.remove(3);
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument greater than size")
+    void removeGreater(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+
+        stack.remove(10);
+        assertTrue(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument greater than size (limited bucket size)")
+    void removeGreater_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+
+        stack.remove(10);
+        assertTrue(stack.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument less than size")
+    void removeLess(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+
+        stack.remove(2);
+        assertFalse(stack.isEmpty());
         assertEquals(1, stack.size());
     }
 
-    // same as remove test but with limited bucket size
-    @Test
-    public void remove_lim() {
-        final RecyclerStack<Object> stack = new RecyclerStack<>(() -> new Object[1], PoolAny.get());
+    @ParameterizedTest
+    @ArgumentsSource(RecyclerStackProvider.class)
+    @DisplayName("can remove with argument less than size (limited bucket size)")
+    void removeLess_lim(@NotNull final RecyclerStack<Integer> stack) {
+        stack.push(0);
+        stack.push(0);
+        stack.push(0);
+        assertFalse(stack.isEmpty());
 
-        // assert method can be called on empty stack
-        assertDoesNotThrow(() -> {
-            stack.remove(10);
-        });
-
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        assertEquals(3, stack.size());
-
-        // assert method can be called on non-empty stack
-        assertDoesNotThrow(() -> stack.remove(10));
-        assertEquals(0, stack.size());
-
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        stack.push(new Object());
-        assertEquals(4, stack.size());
-
-        // assert method removes correct number of elements
-        assertDoesNotThrow(() -> stack.remove(1));
-        assertEquals(3, stack.size());
-        assertDoesNotThrow(() -> stack.remove(2));
+        stack.remove(2);
+        assertFalse(stack.isEmpty());
         assertEquals(1, stack.size());
+    }
+
+    /**
+     * {@code RecyclerStack<Integer>} argument provider for test methods.
+     */
+    static class RecyclerStackProvider implements ArgumentsProvider {
+
+        /**
+         * Provides a {@code RecyclerStack} argument with a limited bucket size
+         * (1) if the test method's name ends in {@code "_lim"}, otherwise the
+         * bucket size is 128.
+         */
+        @Override
+        public Stream<? extends Arguments> provideArguments(@NotNull final ExtensionContext context) {
+            final String methodName = context.getTestMethod().orElseThrow().getName();
+            final RecyclerStack<Integer> stack;
+            if (methodName.endsWith("_lim"))
+                stack = new RecyclerStack<>(() -> new Integer[1], PoolAny.get());
+            else
+                stack = new RecyclerStack<>(() -> new Integer[128], PoolAny.get());
+
+            return Stream.of(stack).map(Arguments::of);
+        }
     }
 }
