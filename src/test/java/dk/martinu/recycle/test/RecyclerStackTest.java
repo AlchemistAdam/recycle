82bf7db8042a0a4420faf2dda0c70927d1d2af19
Adam Martinu <1337programmering.c@gmail.com>
2023-01-01 02:50:28 +0100

    Replaced provider with converter to reduce test cases and duplicate code

--- src/test/java/dk/martinu/recycle/test/RecyclerStackTest.java
+++ src/test/java/dk/martinu/recycle/test/RecyclerStackTest.java
@@ -17,14 +17,15 @@
 
 package dk.martinu.recycle.test;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.converter.*;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.lang.annotation.*;
 import java.util.NoSuchElementException;
-import java.util.stream.Stream;
 
 import dk.martinu.recycle.PoolAny;
 import dk.martinu.recycle.RecyclerStack;
@@ -37,305 +38,199 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("RecyclerStack Test")
 public class RecyclerStackTest {
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("is empty after clear")
-    void emptyAfterClear(@NotNull final RecyclerStack<Integer> stack) {
+    @ParameterizedTest
+    @CsvSource({
+            "def",
+            "lim"
+    })
+    void emptyAfterClear(@NotNull @StackValue final RecyclerStack<Integer> stack) {
         stack.push(0);
         stack.push(0);
         stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(3, stack.size());
 
         stack.clear();
         assertTrue(stack.isEmpty());
         assertEquals(0, stack.size());
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("is empty after clear (limited bucket size)")
-    void emptyAfterClear_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(3, stack.size());
-
-        stack.clear();
-        assertTrue(stack.isEmpty());
-        assertEquals(0, stack.size());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("is empty when new")
-    void isEmpty(@NotNull final RecyclerStack<Integer> stack) {
+    @ParameterizedTest
+    @CsvSource({
+            "def",
+            "lim"
+    })
+    void isEmpty(@NotNull @StackValue final RecyclerStack<Integer> stack) {
         assertTrue(stack.isEmpty());
         assertEquals(0, stack.size());
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("is not empty after pushing")
-    void isNotEmpty(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(1, stack.size());
-
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(2, stack.size());
-
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(3, stack.size());
-
-        stack.push(new Integer[] {0, 0, 0, 0}, 4);
-        assertFalse(stack.isEmpty());
-        assertEquals(7, stack.size());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("is not empty after pushing (limited bucket size)")
-    void isNotEmpty_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(1, stack.size());
-
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(2, stack.size());
-
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-        assertEquals(3, stack.size());
-
-        stack.push(new Integer[] {0, 0, 0, 0}, 4);
-        assertFalse(stack.isEmpty());
-        assertEquals(7, stack.size());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("can pop")
-    void pop(@NotNull final RecyclerStack<Integer> stack) {
+    @ParameterizedTest
+    @CsvSource({
+            "def",
+            "lim"
+    })
+    void pop(@NotNull @StackValue final RecyclerStack<Integer> stack) {
+        // push 1 by 1
         stack.push(0);
         stack.push(1);
         stack.push(2);
-        assertFalse(stack.isEmpty());
-
         assertEquals(2, stack.pop());
         assertEquals(1, stack.pop());
         assertEquals(0, stack.pop());
         assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+
+        // push array
+        stack.push(new Integer[] {3, 4, 5}, 3);
+        assertEquals(5, stack.pop());
+        assertEquals(4, stack.pop());
+        assertEquals(3, stack.pop());
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("can pop array")
-    void popArray(@NotNull final RecyclerStack<Integer> stack) {
-        final Integer[] numbers = {0, 1, 2, 3, 4};
-        final Integer[] rv = new Integer[numbers.length];
+    @ParameterizedTest
+    @CsvSource({
+            "-1, def", // negative
+            " 6, def", // greater than array.length
+            // default
+            "0, def",
+            "2, def",
+            "4, def",
+            // limited
+            "0, lim",
+            "2, lim",
+            "4, lim",
+    })
+    void popArray(final int n, @NotNull @StackValue final RecyclerStack<Integer> stack) {
+        final Integer[] numbers = {0, 1, 2, 3};
+        final Integer[] array = new Integer[numbers.length];
+        final int popCount = Math.min(numbers.length, Math.max(n, 0));
 
+        // push and pop
         stack.push(numbers, numbers.length);
-        assertFalse(stack.isEmpty());
+        stack.pop(array, n);
 
-        assertEquals(numbers.length, stack.pop(rv, rv.length));
-        assertTrue(stack.isEmpty());
-        for (int i = 0; i < rv.length; i++) {
-            final int index = i;
-            assertNotNull(rv[i], () -> String.format("null element at index [%1$d]", index));
-        }
+        // assert correct number of elements are popped from stack
+        assertEquals(numbers.length - popCount, stack.size());
+        if (numbers.length - popCount == 0)
+            assertTrue(stack.isEmpty());
+
+        // assert array contains elements in pop range
+        for (int i = 0; i < popCount; i++)
+            assertNotNull(array[i], String.format("null element at index [%1$d]", i));
+
+        // assert array contains null outside of pop range
+        for (int i = popCount; i < array.length; i++)
+            assertNull(array[i], String.format("not null element at index [%1$d]", i));
     }
 
+    @DisplayName("pop throws NoSuchElementException when empty")
     @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can pop array (limited bucket size)")
-    void popArray_lim(@NotNull final RecyclerStack<Integer> stack) {
-        final Integer[] numbers = {0, 1, 2, 3, 4};
-        final Integer[] rv = new Integer[numbers.length];
-
-        stack.push(numbers, numbers.length);
-        assertFalse(stack.isEmpty());
-
-        assertEquals(numbers.length, stack.pop(rv, rv.length));
-        assertTrue(stack.isEmpty());
-        for (int i = 0; i < rv.length; i++) {
-            final int index = i;
-            assertNotNull(rv[i], () -> String.format("null element at index [%1$d]", index));
-        }
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("pop() throws NoSuchElementException when empty")
-    void popThrowsWhenEmpty(@NotNull final RecyclerStack<Integer> stack) {
+    @CsvSource({
+            "def",
+            "lim"
+    })
+    void popThrowsWhenEmpty(@NotNull @StackValue final RecyclerStack<Integer> stack) {
         assertThrows(NoSuchElementException.class, stack::pop);
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can pop (limited bucket size)")
-    void pop_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(1);
-        stack.push(2);
-        assertFalse(stack.isEmpty());
-
-        assertEquals(2, stack.pop());
-        assertEquals(1, stack.pop());
-        assertEquals(0, stack.pop());
-        assertTrue(stack.isEmpty());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("can push")
-    void push(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
+    @ParameterizedTest
+    @CsvSource({
+            "def",
+            "lim"
+    })
+    void push(@NotNull @StackValue final RecyclerStack<Integer> stack) {
         stack.push(0);
         assertFalse(stack.isEmpty());
+        assertEquals(1, stack.size());
+
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+        assertEquals(2, stack.size());
+
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+        assertEquals(3, stack.size());
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
     @DisplayName("can push array")
-    void pushArray(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(new Integer[] {0, 0, 0, 0}, 4);
-        assertFalse(stack.isEmpty());
-    }
-
     @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can push array (limited bucket size)")
-    void pushArray_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(new Integer[] {0, 0, 0, 0}, 4);
-        assertFalse(stack.isEmpty());
+    @CsvSource({
+            "-1, def", // negative
+            " 6, def", // greater than array.length
+            // default
+            "0, def",
+            "2, def",
+            "4, def",
+            // limited
+            "0, lim",
+            "2, lim",
+            "4, lim",
+    })
+    void pushArray(final int n, @NotNull @StackValue final RecyclerStack<Integer> stack) {
+        final Integer[] array = {0, 0, 0, 0};
+        final int pushCount = Math.min(array.length, Math.max(n, 0));
+
+        stack.push(array, n);
+        assertEquals(pushCount, stack.size());
     }
 
+    @DisplayName("can remove")
     @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can push (limited bucket size)")
-    void push_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
+    @CsvSource({
+            "-1, def", // negative
+            " 6, def", // greater than array.length
+            // default
+            "0, def",
+            "2, def",
+            "4, def",
+            // limited
+            "0, lim",
+            "2, lim",
+            "4, lim",
+    })
+    void remove(final int n, @NotNull @StackValue final RecyclerStack<Integer> stack) {
+        final Integer[] array = {0, 1, 2, 3};
+        final int removeCount = Math.min(array.length, Math.max(n, 0));
+
+        stack.push(array, 4);
+        stack.remove(n);
+
+        // assert correct number of elements are removed from stack
+        assertEquals(array.length - removeCount, stack.size());
+        if (array.length - removeCount == 0)
+            assertTrue(stack.isEmpty());
     }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove when empty")
-    void removeEmpty(@NotNull final RecyclerStack<Integer> stack) {
-        stack.remove(10);
-        assertTrue(stack.isEmpty());
-    }
+    @Target({ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
+    @Retention(RetentionPolicy.RUNTIME)
+    @ConvertWith(StringToStackConverter.class)
+    public @interface StackValue { }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument equal to size")
-    void removeExact(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
+    @SuppressWarnings("rawtypes") // cannot select from parameterized class
+    public static class StringToStackConverter extends TypedArgumentConverter<String, RecyclerStack> {
 
-        stack.remove(3);
-        assertTrue(stack.isEmpty());
-        assertEquals(0, stack.size());
-    }
+        protected StringToStackConverter() {
+            super(String.class, RecyclerStack.class);
+        }
 
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument equal to size (limited bucket size)")
-    void removeExact_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-
-        stack.remove(3);
-        assertTrue(stack.isEmpty());
-        assertEquals(0, stack.size());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument greater than size")
-    void removeGreater(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-
-        stack.remove(10);
-        assertTrue(stack.isEmpty());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument greater than size (limited bucket size)")
-    void removeGreater_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-
-        stack.remove(10);
-        assertTrue(stack.isEmpty());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument less than size")
-    void removeLess(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-
-        stack.remove(2);
-        assertFalse(stack.isEmpty());
-        assertEquals(1, stack.size());
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(RecyclerStackProvider.class)
-    @DisplayName("can remove with argument less than size (limited bucket size)")
-    void removeLess_lim(@NotNull final RecyclerStack<Integer> stack) {
-        stack.push(0);
-        stack.push(0);
-        stack.push(0);
-        assertFalse(stack.isEmpty());
-
-        stack.remove(2);
-        assertFalse(stack.isEmpty());
-        assertEquals(1, stack.size());
-    }
-
-    /**
-     * {@code RecyclerStack<Integer>} argument provider for test methods.
-     */
-    static class RecyclerStackProvider implements ArgumentsProvider {
-
-        /**
-         * Provides a {@code RecyclerStack} argument with a limited bucket size
-         * (1) if the test method's name ends in {@code "_lim"}, otherwise the
-         * bucket size is 128.
-         */
+        @Contract(value = "_ -> new", pure = true)
+        @NotNull
         @Override
-        public Stream<? extends Arguments> provideArguments(@NotNull final ExtensionContext context) {
-            final String methodName = context.getTestMethod().orElseThrow().getName();
-            final RecyclerStack<Integer> stack;
-            if (methodName.endsWith("_lim"))
-                stack = new RecyclerStack<>(x -> new Integer[1], PoolAny.get());
-            else
-                stack = new RecyclerStack<>(x -> new Integer[128], PoolAny.get());
-
-            return Stream.of(stack).map(Arguments::of);
+        protected RecyclerStack<Integer> convert(final String source) throws ArgumentConversionException {
+            return switch (source) {
+                // default
+                case "def" -> new RecyclerStack<>(x -> new Integer[128], PoolAny.get());
+                // limited bucket size
+                case "lim" -> new RecyclerStack<>(x -> new Integer[1], PoolAny.get());
+                // cannot convert to stack
+                default -> throw new ArgumentConversionException("invalid stack source {" + source + "}");
+            };
         }
     }
 }
